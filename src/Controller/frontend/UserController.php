1eb43241335b131135609995f572dfdbab0dbda1
kolera75 <amine.sebki@gmail.com>
2022-04-20 19:06:28 +0200

    Ajout de l'inscription

--- src/Controller/frontend/UserController.php
+++ src/Controller/frontend/UserController.php
@@ -96,3 +96,4 @@ class UserController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
+
